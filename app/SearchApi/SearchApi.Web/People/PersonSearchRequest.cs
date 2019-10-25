299c341e9c9339b8fa04fda56d5bf4aa8be2c183
alex <alexandre.joyeux@nttdata.com>
2019-10-25 14:13:45 -0700

    Adding PersonSearchRequest model

---
+++ app/SearchApi/SearchApi.Web/People/PersonSearchRequest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel;
+using Newtonsoft.Json;
+
+namespace SearchApi.Web.Controllers
+{
+    /// <summary>
+    /// The PersonSearchRequest represents the information known about a subject before executing a search.
+    /// </summary>
+    [Description("Represents a set of information to execute a search on a person")]
+    public class PersonSearchRequest
+    {
+        [JsonConstructor]
+        public PersonSearchRequest(string firstName, string lastName, DateTime? dateOfBirth)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.DateOfBirth = dateOfBirth;
+        }
+
+        [Description("The first name of the subject.")]
+        public String FirstName { get; }
+        [Description("The last name of the subject.")]
+        public String LastName { get; }
+        [Description("The date of birth of the subject.")]
+        public DateTime? DateOfBirth { get; }
+    }
+}

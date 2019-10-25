39cc8b114d5118384719497ce7b5af0651bb3d07
alex <alexandre.joyeux@nttdata.com>
2019-10-25 13:41:06 -0700

    Creating PeopleController

---
+++ app/SearchApi/SearchApi.Web/Controllers/PeopleController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SearchApi.Web.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        [HttpPost]
+        [Route("search")]
+        public async Task<IActionResult> Search()
+        {
+            return await Task.FromResult(Ok());
+        }
+    }
+}

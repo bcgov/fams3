907ce5efef98f815b4eb91cace20f23103457a8e
alex <alexandre.joyeux@nttdata.com>
2019-10-28 09:03:11 -0700

    Add test for PeopleController

--- app/SearchApi/SearchApi.Web.Test/People/PeopleControllerTest.cs
+++ app/SearchApi/SearchApi.Web.Test/People/PeopleControllerTest.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Moq;
 using NUnit.Framework;
 using NUnit.Framework.Internal;
@@ -16,13 +21,21 @@ namespace SearchApi.Web.Test.People
 
         private readonly Mock<ISpan> _spanMock = new Mock<ISpan>();
 
-        [SetUp]
+        private readonly Mock<ILogger<PeopleController>> _loggerMock = new Mock<ILogger<PeopleController>>();
+
+        private readonly Mock<ISendEndpoint> _sendEndpointMock = new Mock<ISendEndpoint>();
+
+        private readonly Mock<ISendEndpointProvider> _sendEndPointProviderMock = new Mock<ISendEndpointProvider>();
+
+            [SetUp]
         public void Init()
         {
 
+            EndpointConvention.Map<ExecuteSearch>(new Uri("http://random"));
+            _sendEndPointProviderMock.Setup(x => x.GetSendEndpoint(It.IsAny<Uri>())).Returns(Task.FromResult(_sendEndpointMock.Object));
             _spanMock.Setup(x => x.SetTag(It.IsAny<string>(), It.IsAny<string>())).Returns(_spanMock.Object);
             _tracerMock.Setup(x => x.ActiveSpan).Returns(_spanMock.Object);
-            _sut = new PeopleController(null, _tracerMock.Object);
+            _sut = new PeopleController(_sendEndPointProviderMock.Object, _loggerMock.Object, _tracerMock.Object);
         }
 
 
@@ -34,7 +47,7 @@ namespace SearchApi.Web.Test.People
             Assert.IsInstanceOf<PersonSearchResponse>(result.Value);
             Assert.IsNotNull(((PersonSearchResponse)result.Value).Id);
             _spanMock.Verify(x => x.SetTag("searchRequestId", $"{((PersonSearchResponse)result.Value).Id}"), Times.Once);
-
+            _sendEndpointMock.Verify(x => x.Send(It.IsAny<ExecuteSearch>(), It.IsAny<CancellationToken>()), Times.Once);
         }
 
     }
--- app/SearchApi/SearchApi.Web/People/PeopleController.cs
+++ app/SearchApi/SearchApi.Web/People/PeopleController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using MassTransit;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using NSwag;
 using NSwag.Annotations;
 using OpenTracing;
@@ -19,18 +20,22 @@ namespace SearchApi.Web.Controllers
     public class PeopleController : ControllerBase
     {
 
-        private ITracer _tracer;
+        private readonly ITracer _tracer;
 
-        private IBus _bus;
+        private readonly ISendEndpointProvider _sendEndpointProvider;
 
-        public PeopleController(IBus bus, ITracer tracer)
+        private readonly ILogger _logger;
+
+        public PeopleController(ISendEndpointProvider sendEndpointProvider, ILogger<PeopleController> logger, ITracer tracer)
         {
-            this._bus = bus;
+            this._sendEndpointProvider = sendEndpointProvider;
+            this._logger = logger;
             this._tracer = tracer;
         }
 
         /// <summary>
-        /// Receives a <see cref="PersonSearchRequest"/> and start the process of searching additional information for a person.
+        /// Receives a <see cref="PersonSearchRequest"/> and start the process of searching additional information for a person
+        /// by submitting an ExecuteSearch Command to appropriate queue.
         /// </summary>
         /// <param name="personSearchRequest">A request to search a person</param>
         /// <returns><see cref="PersonSearchResponse"/></returns>
@@ -43,15 +48,19 @@ namespace SearchApi.Web.Controllers
         { 
             Guid searchRequestId = Guid.NewGuid();
 
+            _logger.LogInformation($"Successfully received new search request [{searchRequestId}].");
+
             _tracer.ActiveSpan.SetTag("searchRequestId", $"{searchRequestId}");
 
-            await this._bus.Send<ExecuteSearch>(new
+            _logger.LogDebug($"Attempting to send {nameof(ExecuteSearch)} to destination queue.");
+            await this._sendEndpointProvider.Send<ExecuteSearch>(new
             {
                 Id = searchRequestId,
                 personSearchRequest.FirstName,
                 personSearchRequest.LastName,
                 personSearchRequest.DateOfBirth
             });
+            _logger.LogInformation($"Successfully sent {nameof(ExecuteSearch)} to destination queue.");
 
             return Accepted(new PersonSearchResponse(searchRequestId));
         }

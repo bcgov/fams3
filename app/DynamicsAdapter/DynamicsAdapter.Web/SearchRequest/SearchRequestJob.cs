96f28ee85f0231abe6b9e5d83f1d9c7858947589
alex <alexandre.joyeux@nttdata.com>
2019-10-28 12:06:54 -0700

    Add SearchRequestJob

---
+++ app/DynamicsAdapter/DynamicsAdapter.Web/SearchRequest/SearchRequestJob.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Logging;
+using Quartz;
+using System.Threading.Tasks;
+
+/// <summary>
+/// The SearchRequestJob orchestrates dyanmics search request.
+/// </summary>
+[DisallowConcurrentExecution]
+public class SearchRequestJob : IJob
+{
+    private readonly ILogger<SearchRequestJob> _logger;
+    public SearchRequestJob(ILogger<SearchRequestJob> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task Execute(IJobExecutionContext context)
+    {
+        _logger.LogInformation("Search Request started!");
+        return Task.CompletedTask;
+    }
+}

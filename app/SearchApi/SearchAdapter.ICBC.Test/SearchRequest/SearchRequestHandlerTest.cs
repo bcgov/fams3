a5359fb4085f401787ce1efb13f846106d78b0c5
alex <alexandre.joyeux@nttdata.com>
2019-10-29 14:49:37 -0700

    Adding unit tests

---
+++ app/SearchApi/SearchAdapter.ICBC.Test/SearchRequest/SearchRequestHandlerTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using MassTransit;
+using MassTransit.Context;
+using MassTransit.Testing;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using SearchAdapter.ICBC.SearchRequest;
+using SearchApi.Core.Contracts;
+
+namespace SearchAdapter.ICBC.Test.SearchRequest
+{
+    public class SearchRequestHandlerTest
+    {
+
+        private InMemoryTestHarness _harness;
+        private ConsumerTestHarness<SearchRequestHandler> _sut;
+
+        private readonly Mock<ILogger<SearchRequestHandler>> _loggerMock = new Mock<ILogger<SearchRequestHandler>>();
+
+        [OneTimeSetUp]
+        public async Task A_consumer_is_being_tested()
+        {
+            _harness = new InMemoryTestHarness();
+            _sut = _harness.Consumer(() => new SearchRequestHandler(_loggerMock.Object));
+
+            await _harness.Start();
+
+            await _harness.InputQueueSendEndpoint.Send<ExecuteSearch>(new
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "firstName",
+                LastName = "lastName",
+                DateOfBirth = new DateTime(2001, 1, 1)
+            });
+
+        }
+
+
+        [OneTimeTearDown]
+        public async Task Teardown()
+        {
+            await _harness.Stop();
+        }
+
+        [Test]
+        public void Should_send_the_initial_message_to_the_consumer()
+        {
+            Assert.IsTrue(_harness.Sent.Select<ExecuteSearch>().Any());
+        }
+
+        [Test]
+        public void Should_receive_the_message_type_a()
+        {
+            Assert.IsTrue(_harness.Consumed.Select<ExecuteSearch>().Any());
+        }
+
+
+    }
+}
---
+++ app/SearchApi/SearchApi.Core.Test/Configuration/RabbitMqConfigurationTest.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using SearchApi.Core.Configuration;
+
+namespace SearchApi.Core.Test.Configuration
+{
+    public class RabbitMqConfigurationTest
+    {
+        [Test]
+        public void With_no_param_should_set_default_rabbit_config()
+        {
+            var sut = new RabbitMqConfiguration();
+
+            Assert.AreEqual("localhost", sut.Host);
+            Assert.AreEqual(5672, sut.Port);
+            Assert.AreEqual("guest", sut.Username);
+            Assert.AreEqual("guest", sut.Password);
+
+        }
+
+        [Test]
+        public void With_param_should_configure_rabbitMq()
+        {
+            var sut = new RabbitMqConfiguration()
+            {
+                Host = "host",
+                Port = 6666,
+                Username = "username",
+                Password = "pwd"
+            };
+
+            Assert.AreEqual("host", sut.Host);
+            Assert.AreEqual(6666, sut.Port);
+            Assert.AreEqual("username", sut.Username);
+            Assert.AreEqual("pwd", sut.Password);
+
+        }
+    }
+}

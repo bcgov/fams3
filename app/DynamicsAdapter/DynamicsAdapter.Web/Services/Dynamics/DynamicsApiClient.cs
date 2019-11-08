bf03b653d0f96939e64cdbc8dc824e12c4ede0b8
alex <alexandre.joyeux@nttdata.com>
2019-11-08 09:40:39 -0800

    remove token

--- app/DynamicsAdapter/DynamicsAdapter.Web/Services/Dynamics/DynamicsApiClient.cs
+++ app/DynamicsAdapter/DynamicsAdapter.Web/Services/Dynamics/DynamicsApiClient.cs
@@ -38,9 +38,6 @@ namespace DynamicsAdapter.Web.Services.Dynamics
 
             using (var request = new HttpRequestMessage(HttpMethod.Get, endpoint))
             {
-
-                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6InBSTW1LWUVMQVpsYnVRRllnNGNacmppTW02NCJ9.eyJhdWQiOiJodHRwczovL2pzYi1mYW1zLmRldi5qYWcuZ292LmJjLmNhL2FwaS9kYXRhL3Y5LjAvIiwiaXNzIjoiaHR0cDovL3N0czQuZ292LmJjLmNhL2FkZnMvc2VydmljZXMvdHJ1c3QiLCJpYXQiOjE1NzMxNTUyNjgsImV4cCI6MTU3MzE1ODg2OCwidXBuIjoiYWxleC5qb3lldXhAZ292LmJjLmNhIiwidW5pcXVlX25hbWUiOiJJRElSXFxham95ZXV4IiwiYXBwdHlwZSI6IkNvbmZpZGVudGlhbCIsImFwcGlkIjoiM2ExNGQyNzYtNWUzMS00MWI0LWJjOTEtMzJiZTMyODA2OTRmIiwiYXV0aG1ldGhvZCI6InVybjpvYXNpczpuYW1lczp0YzpTQU1MOjIuMDphYzpjbGFzc2VzOlBhc3N3b3JkUHJvdGVjdGVkVHJhbnNwb3J0IiwiYXV0aF90aW1lIjoiMjAxOS0xMS0wN1QxOTozNDoyOC4yODZaIiwidmVyIjoiMS4wIiwic2NwIjoib3BlbmlkIn0.I8szg0M0YBn_4UzmuRhFKnAqVAMR7hDPP32Ukrqi9_F5i-q4Wor3_As-frVzPPjHw8QWPYTcFhlz5gfVPiuoLn8byqWofntWtVxV6L2SiTnulwZEes9CndvHcrP3emRV8zPOIw_rm80axbh1RPrfiRmQtdj9Q4zQ91ph3yCMvokDX_ul3bP-CivioTlVvJe49EyT1YCCW3_7Ykj2hZpqPHseBBoloWIFLeroet0grXsIBe41vzGlHPHVKmyQuEISJ-Q-w5ih5_bmYVz4uFU96PJzv0_qD38xFrF_C9p_wBKV-QvnEHvgurKG10XQ6n6_Ua9vBne86-oGRXp9EzsjAw");
-
                 var response = await _httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead,
                     CancellationToken.None);
 

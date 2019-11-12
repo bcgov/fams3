42be84ec99eaeca92b1a929fa0af964d5f1ddaa9
peggy <peggy.zhang@gov.bc.ca>
2019-11-12 12:43:53 -0800

    remove unused reference

--- app/DynamicsAdapter/DynamicsAdapter.Web/MatchFound/MatchFoundController.cs
+++ app/DynamicsAdapter/DynamicsAdapter.Web/MatchFound/MatchFoundController.cs
@@ -5,7 +5,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using OpenTracing;
 
 namespace DynamicsAdapter.Web.MatchFound
 {

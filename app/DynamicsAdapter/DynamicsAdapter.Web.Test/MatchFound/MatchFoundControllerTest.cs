91e61f045583fb0132e6b2ee042aa948f1fd2b83
peggy <peggy.zhang@gov.bc.ca>
2019-11-12 12:38:26 -0800

    Create new post endpoint in dynamics adapter to receive match found events

---
+++ app/DynamicsAdapter/DynamicsAdapter.Web.Test/MatchFound/MatchFoundControllerTest.cs
@@ -0,0 +1,47 @@
+﻿using DynamicsAdapter.Web.MatchFound;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DynamicsAdapter.Web.Test.MatchFound
+{
+    public class MatchFoundControllerTest
+    {
+        private MatchFoundController _sut;
+        private readonly Mock<ILogger<MatchFoundController>> _loggerMock = new Mock<ILogger<MatchFoundController>>();
+
+        [SetUp]
+        public void Init()
+        {
+            _sut = new MatchFoundController(_loggerMock.Object);
+        }
+
+        [Test]
+        public void with_valid_match_found_data_should_return_ok()
+        {
+            MatchFoundResponse matchFoundResponse = new MatchFoundResponse
+            {
+                SearchRequestId = "searchRequestGuid",
+                SearchResult = "searchResult"
+            };
+            IActionResult result =(OkResult) this._sut.MatchFound(matchFoundResponse).Result;
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void with_invalid_match_found_data_should_return_bad_request()
+        {
+            MatchFoundResponse matchFoundResponse = new MatchFoundResponse
+            {
+                SearchRequestId = "",
+                SearchResult = "searchResult"
+            };
+            IActionResult result = (BadRequestResult)this._sut.MatchFound(matchFoundResponse).Result;
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ app/DynamicsAdapter/DynamicsAdapter.Web/MatchFound/MatchFoundController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OpenTracing;
+
+namespace DynamicsAdapter.Web.MatchFound
+{
+    public class MatchFoundResponse
+    { 
+        public string SearchRequestId { get; set; }
+        public string SearchResult { get; set; }
+    }
+
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MatchFoundController : ControllerBase
+    {
+        private readonly ILogger<MatchFoundController> _logger;
+        public MatchFoundController(ILogger<MatchFoundController> logger)
+        {
+            _logger = logger;
+        }
+
+        //POST
+        [HttpPost]
+        public async Task<IActionResult> MatchFound(MatchFoundResponse response)
+        { 
+            if(String.IsNullOrEmpty(response.SearchRequestId))
+            {
+                return BadRequest();
+            }
+            _logger.LogInformation("Received MatchFound response with SearchRequestId is " + response.SearchRequestId);
+            _logger.LogInformation("SearchResult is " + response.SearchResult);
+            return Ok();
+        }
+    }
+}

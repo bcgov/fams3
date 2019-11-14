44759c2f0a98ca5292265890da3ba5d64ba3f4f2
rantifamilusi <familusi.babaranti@gmail.com>
2019-11-14 19:15:37 +0100

    add health check for status reason

---
+++ app/DynamicsAdapter/DynamicsAdapter.Web/Health/StatusReasonHealthCheck.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace DynamicsAdapter.Web.Health
+{
+    public class StatusReasonHealthCheck : IHealthCheck
+    {
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = new CancellationToken())
+        {
+            var healthCheckResultHealthy = true;
+
+            if (healthCheckResultHealthy)
+            {
+                return Task.FromResult(
+                    HealthCheckResult.Healthy("A healthy result."));
+            }
+
+            return Task.FromResult(
+                HealthCheckResult.Unhealthy("An unhealthy result."));
+        }
+    }
+}

b46cbf2d963b78f456b75b3c4cae301c65104165
peggy <peggy.zhang@gov.bc.ca>
2019-11-25 15:50:57 -0800

    typing issue

--- app/SearchApi/SearchApi.Web/People/Identifier.cs
+++ app/SearchApi/SearchApi.Web/People/Identifier.cs
@@ -17,14 +17,19 @@ namespace SearchApi.Web.People
 
         [Description("The identification serial number of this identifier.")]
         public string Identification { get; set; }
+
         [Description("The identification effective date.")]
         public DateTime? IdentificationEffectiveDate { get; set; }
+
         [Description("The identification expiration date.")]
         public DateTime? IdentificationExpirationDate { get; set; }
+
         [Description("The identfication type,such as driver license.")]
-        public int IdentfierType { get; set; }
+        public int IdentifierType { get; set; }
+
         [Description("The inforation source of this identifier, such as icbc.")]
         public int InformationSourceType{ get; set; }
+
         [Description("The identifier is issued by who or which organization.")]
         public string IssuedBy { get; set; }
     }

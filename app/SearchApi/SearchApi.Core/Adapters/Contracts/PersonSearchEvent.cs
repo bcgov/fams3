bea181dc72a7161a613abd9be64e0ba964ad1e02
alex <alexandre.joyeux@nttdata.com>
2019-11-25 12:18:08 -0800

    Added personSearch rejected event

---
+++ app/SearchApi/SearchApi.Core/Adapters/Contracts/PersonSearchEvent.cs
@@ -0,0 +1,7 @@
+﻿namespace SearchApi.Core.Adapters.Contracts
+{
+    public class PersonSearchEvent
+    {
+        
+    }
+}
---
+++ app/SearchApi/SearchApi.Core/Adapters/Contracts/PersonSearchFailed.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SearchApi.Core.Adapters.Contracts
+{
+    public interface PersonSearchFailed : PersonSearchEvent
+    {
+        Exception Cause { get; }
+    }
+}

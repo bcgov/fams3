9aa0a0dc9911859e0797ff91974d24edcb6657ff
peggy <peggy.zhang@gov.bc.ca>
2019-11-25 15:26:41 -0800

    Auto stash before rebase of "bcgov/master"

--- app/DynamicsAdapter/Fams3Adapter.Dynamics/Identifier/IdentificationType.cs
+++ app/DynamicsAdapter/Fams3Adapter.Dynamics/Identifier/IdentificationType.cs
@@ -5,19 +5,24 @@ namespace Fams3Adapter.Dynamics.Identifier
     public class IdentificationType : Enumeration
     {
 
-        public static IdentificationType DriverLicense = new IdentificationType(867670000, "Driver's License");
-        public static IdentificationType SocialInsuranceNumber = new IdentificationType(867670006, "Social Insurance Number");
-        public static IdentificationType PersonalHealthNumber = new IdentificationType(867670001, "Personal Health Number");
-        public static IdentificationType BirthCertificate = new IdentificationType(867670008, "Birth Certificate");
-        public static IdentificationType CorrectionsId = new IdentificationType(867670009, "Corrections ID");
-        public static IdentificationType NativeStatusCard = new IdentificationType(867670011, "Native Status Card");
-        public static IdentificationType Passport = new IdentificationType(867670002, "Passport");
-        public static IdentificationType WcbClaim = new IdentificationType(867670015, "WCB Claim");
-        public static IdentificationType Other = new IdentificationType(867670012, "Other");
-        public static IdentificationType SecurityKeyword = new IdentificationType(867670013, "Security Keyword");
+        public static IdentificationType DriverLicense = new IdentificationType(867670000, 1, "Driver's License");
+        public static IdentificationType SocialInsuranceNumber = new IdentificationType(867670006, 2, "Social Insurance Number");
+        public static IdentificationType PersonalHealthNumber = new IdentificationType(867670001, 3,  "Personal Health Number");
+        public static IdentificationType BirthCertificate = new IdentificationType(867670008, 4, "Birth Certificate");
+        public static IdentificationType CorrectionsId = new IdentificationType(867670009, 5, "Corrections ID");
+        public static IdentificationType NativeStatusCard = new IdentificationType(867670011, 6, "Native Status Card");
+        public static IdentificationType Passport = new IdentificationType(867670002, 7, "Passport");
+        public static IdentificationType WcbClaim = new IdentificationType(867670015, 8, "WCB Claim");
+        public static IdentificationType Other = new IdentificationType(867670012, 9, "Other");
+        public static IdentificationType SecurityKeyword = new IdentificationType(867670013, 10, "Security Keyword");
 
-        protected IdentificationType(int value, string name) : base(value, name)
+        public int DynamicValue { get; private set; }
+        public int SearchApiValue { get; private set; }
+
+        protected IdentificationType(int dynamicValue, int searchApiValue, string name) : base(dynamicValue, name)
         {
         }
+
+
     }
 }

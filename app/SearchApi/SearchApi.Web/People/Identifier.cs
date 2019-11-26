1ad7423919a8ac0e2cc81c63c77e88e5da2b44ce
peggy <peggy.zhang@gov.bc.ca>
2019-11-26 15:42:37 -0800

    updates

--- app/SearchApi/SearchApi.Web/People/Identifier.cs
+++ app/SearchApi/SearchApi.Web/People/Identifier.cs
@@ -10,7 +10,6 @@ namespace SearchApi.Web.People
     [Description("Represents a set of information to execute a search on a person")]
     public class Identifier
     {
-<<<<<<< HEAD
         [Description("The identification serial number of this identifier.")]
         public string SerialNumber { get; set; }
 
@@ -25,27 +24,6 @@ namespace SearchApi.Web.People
 
         [Description("The inforation source of this identifier, such as icbc.")]
         public IdentifierSourceEnum Source { get; set; }
-=======
-        [JsonConstructor]
-        public Identifier()
-        {
-        }
-
-        [Description("The identification serial number of this identifier.")]
-        public string Identification { get; set; }
-
-        [Description("The identification effective date.")]
-        public DateTime? IdentificationEffectiveDate { get; set; }
-
-        [Description("The identification expiration date.")]
-        public DateTime? IdentificationExpirationDate { get; set; }
-
-        [Description("The identfication type,such as driver license.")]
-        public int IdentifierType { get; set; }
-
-        [Description("The inforation source of this identifier, such as icbc.")]
-        public int InformationSourceType{ get; set; }
->>>>>>> transferIdentitiesToSearchApi
 
         [Description("The identifier is issued by who or which organization.")]
         public string IssuedBy { get; set; }

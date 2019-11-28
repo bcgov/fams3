ff959a0e6b162f7f1b2cd6f570975a6a1f3b5637
alex <alexandre.joyeux@nttdata.com>
2019-11-28 15:47:58 -0800

    removed unused fake item

--- app/SearchApi/SearchApi.Web.Test/Notifications/WebHookNotifierSearchStatusTest.cs
+++ app/SearchApi/SearchApi.Web.Test/Notifications/WebHookNotifierSearchStatusTest.cs
@@ -203,7 +203,6 @@ namespace SearchApi.Web.Test.Notifications
             [Test]
             public async Task it_should_log_error_when_not_uri()
             {
-                var fakeMatchFound = new FakePersonFound();
 
                 _searchApiOptionsMock.Setup(x => x.Value).Returns(new SearchApiOptions()
                     .AddWebHook("test", "not_uri"));

a4eff0f6583c8edf523253466573b4f94094ac7f
alex <alexandre.joyeux@nttdata.com>
2019-12-02 10:42:52 -0800

    Added support to send failure

--- app/SearchApi/SearchApi.Core/OpenTracing/OpenTracingPublishFilter.cs
+++ app/SearchApi/SearchApi.Core/OpenTracing/OpenTracingPublishFilter.cs
@@ -13,22 +13,29 @@ namespace SearchApi.Core.OpenTracing
 
         public async Task Send(PublishContext context, IPipe<PublishContext> next)
         {
-            var operationName = $"Publishing Message: {context.DestinationAddress.GetExchangeName()}";
-
-            var spanBuilder = GlobalTracer.Instance.BuildSpan(operationName)
-               .AsChildOf(GlobalTracer.Instance.ActiveSpan.Context)
-               .WithTag("destination-address", context.DestinationAddress?.ToString())
-               .WithTag("source-address", context.SourceAddress?.ToString())
-               .WithTag("initiator-id", context.InitiatorId?.ToString())
-               .WithTag("message-id", context.MessageId?.ToString());
-
-            using (var scope = spanBuilder.StartActive())
+            try
             {
-                GlobalTracer.Instance.Inject(
-                   GlobalTracer.Instance.ActiveSpan.Context,
-                   BuiltinFormats.TextMap,
-                   new MassTransitPublishContextTextMapInjectAdapter(context));
+                var operationName = $"Publishing Message: {context.DestinationAddress.GetExchangeName()}";
 
+                var spanBuilder = GlobalTracer.Instance.BuildSpan(operationName)
+                    .AsChildOf(GlobalTracer.Instance.ActiveSpan.Context)
+                    .WithTag("destination-address", context.DestinationAddress?.ToString())
+                    .WithTag("source-address", context.SourceAddress?.ToString())
+                    .WithTag("initiator-id", context.InitiatorId?.ToString())
+                    .WithTag("message-id", context.MessageId?.ToString());
+
+                using (var scope = spanBuilder.StartActive())
+                {
+                    GlobalTracer.Instance.Inject(
+                        GlobalTracer.Instance.ActiveSpan.Context,
+                        BuiltinFormats.TextMap,
+                        new MassTransitPublishContextTextMapInjectAdapter(context));
+
+                    await next.Send(context);
+                }
+            }
+            catch
+            {
                 await next.Send(context);
             }
         }

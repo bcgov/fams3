ff02ffe68fc16f77cce13fd563f1f357c88cde5c
alex <alex.joyeux@gov.bc.ca>
2019-12-03 13:15:08 -0800

    Added support for absolute urls

--- app/DynamicsAdapter/DynamicsAdapter.Web/ApiGateway/ApiGatewayHandler.cs
+++ app/DynamicsAdapter/DynamicsAdapter.Web/ApiGateway/ApiGatewayHandler.cs
@@ -24,8 +24,27 @@ namespace DynamicsAdapter.Web.ApiGateway
         {
             if(string.IsNullOrEmpty(_apiGatewayOptions.BasePath)) return await base.SendAsync(request, cancellationToken);
 
-            request.RequestUri = new Uri(new Uri(_apiGatewayOptions.BasePath), request.RequestUri.AbsolutePath);
+            if (Uri.TryCreate(CombineUrls(_apiGatewayOptions.BasePath, request.RequestUri.AbsolutePath),  UriKind.Absolute, out var path))
+            {
+                request.RequestUri = path;
+            }
+            
             return await base.SendAsync(request, cancellationToken);
+        }
+
+
+        public static string CombineUrls(string baseUrl, string relativeUrl)
+        {
+            if (string.IsNullOrWhiteSpace(baseUrl))
+                throw new ArgumentNullException(nameof(baseUrl));
+
+            if (string.IsNullOrWhiteSpace(relativeUrl))
+                return baseUrl;
+
+            baseUrl = baseUrl.TrimEnd('/');
+            relativeUrl = relativeUrl.TrimStart('/');
+
+            return $"{baseUrl}/{relativeUrl}";
         }
     }
 }

38005c24c2e22cbb174e9df55c2ce94546dad78b
Ranti Familusi <ranti.familusi@nttdata.com>
2019-12-11 17:11:56 -0700

    add person phone number contract and updated person contact.

---
+++ app/SearchApi/SearchApi.Core/Person/Contracts/PersonPhoneNumber.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SearchApi.Core.Person.Contracts
+{
+    public interface PersonPhoneNumber
+    {
+        string SuppliedBy { get; }
+
+        string EffectiveDate { get; }
+
+        string EffectiveDateType { get; }
+
+        string PhoneNumber { get; }
+
+        string PhoneNumberType { get; }
+    }
+}

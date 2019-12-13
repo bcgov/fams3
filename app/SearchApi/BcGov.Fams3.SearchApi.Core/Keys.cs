fd27420edeed1eeb4fedc403193f25d277a5ecf2
alex <alex.joyeux@gov.bc.ca>
2019-12-13 12:56:51 -0800

    Fixed queue configuration for providers

---
+++ app/SearchApi/BcGov.Fams3.SearchApi.Core/Keys.cs
@@ -0,0 +1,8 @@
+﻿namespace BcGov.Fams3.SearchApi.Core
+{
+    public class Keys
+    {
+        internal const string PROVIDER_SECTION_SETTING_KEY = "ProviderProfile";
+        internal const string RABBITMQ_SECTION_SETTING_KEY = "RabbitMq";
+    }
+}

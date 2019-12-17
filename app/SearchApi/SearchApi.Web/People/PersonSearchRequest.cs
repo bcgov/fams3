8f1eda3ae2c7eb4b94cbd53ef9980f8c2988b4be
alex <alex.joyeux@gov.bc.ca>
2019-12-17 09:38:03 -0800

    Fixed search Api

--- app/SearchApi/SearchApi.Web/People/PersonSearchRequest.cs
+++ app/SearchApi/SearchApi.Web/People/PersonSearchRequest.cs
@@ -17,24 +17,32 @@ namespace SearchApi.Web.Controllers
     {
 
         [JsonConstructor]
-        public PersonSearchRequest(string firstName, string lastName, DateTime? dateOfBirth, IEnumerable<SearchApiPersonalIdentifier> identifiers)
+        public PersonSearchRequest(
+            string firstName, 
+            string lastName, 
+            DateTime? dateOfBirth, 
+            IEnumerable<SearchApiPersonalIdentifier> identifiers,
+            IEnumerable<SearchApiAddress> addresses,
+            IEnumerable<SearchApiPhoneNumber> phoneNumbers)
         {
             this.FirstName = firstName;
             this.LastName = lastName;
             this.DateOfBirth = dateOfBirth;
             this.Identifiers = identifiers;
+            this.PhoneNumbers = phoneNumbers;
         }
 
-        [Description("The first name of the subject.")]
+        [Description("The first name of the subject")]
         public string FirstName { get; }
-        [Description("The last name of the subject.")]
+        [Description("The last name of the subject")]
         public string LastName { get; }
-        [Description("The date of birth of the subject.")]
+        [Description("The date of birth of the subject")]
         public DateTime? DateOfBirth { get; }
-
+        [Description("A collection of Personal Identifiers")]
         public IEnumerable<PersonalIdentifier> Identifiers { get; set; }
+        [Description("A collection of addresses")]
         public IEnumerable<Address> Addresses { get; set; }
-
+        [Description("A collection of phone numbers")]
         public IEnumerable<PhoneNumber> PhoneNumbers { get; set; }
 
 
@@ -43,15 +51,50 @@ namespace SearchApi.Web.Controllers
 
     public class SearchApiPersonalIdentifier : PersonalIdentifier
     {
-        [Description("The serial number of the identifier.")]
+        [Description("The serial number of the identifier")]
         public string SerialNumber { get; set; }
-        [Description("The effective date of the identifier.")]
+        [Description("The effective date of the identifier")]
         public DateTime? EffectiveDate { get; set; }
-        [Description("The expiration date of the identifier.")]
+        [Description("The expiration date of the identifier")]
         public DateTime? ExpirationDate { get; set; }
-        [Description("The type of the identifier.")]
+        [Description("The type of the identifier")]
         public PersonalIdentifierType Type { get; set; }
-        [Description("The issuer of the identifier.")]
+        [Description("The issuer of the identifier")]
         public string IssuedBy { get; set; }
+    }
+
+
+    public class SearchApiAddress : Address
+    {
+        [Description("The type of address")]
+        public string Type { get; }
+        [Description("The Address Line 1")]
+        public string AddressLine1 { get; }
+        [Description("The Address Line 2")]
+        public string AddressLine2 { get; }
+        [Description("The Address Province or state")]
+        public string Province { get; }
+        [Description("The Address City")]
+        public string City { get; }
+        [Description("The Address Country")]
+        public string Country { get; }
+        [Description("The Address Zip or Postal Code")]
+        public string PostalCode { get; }
+        public string NonCanadianState { get; }
+        public string SuppliedBy { get; }
+    }
+
+
+    public class SearchApiPhoneNumber : PhoneNumber
+    {
+        public string SuppliedBy { get; }
+        [Description("A Date")]
+        public DateTime? Date { get; }
+        [Description("The Date type of the supplied Date")]
+        public string DateType { get; }
+        [Description("The Phone number")]
+        public string PhoneNumber { get; }
+        [Description("The phone number type")]
+        public string PhoneNumberType { get; }
     }
 }

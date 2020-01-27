3c158f1d700e8a415abb5b58ee0353a3882c871e
Ranti Familusi <ranti.familusi@nttdata.com>
2020-01-27 15:36:30 -0700

    person model extended

--- app/SearchApi/BcGov.Fams3.SearchApi.Contracts/Person/Person.cs
+++ app/SearchApi/BcGov.Fams3.SearchApi.Contracts/Person/Person.cs
@@ -7,7 +7,17 @@ namespace BcGov.Fams3.SearchApi.Contracts.Person
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public string OtherName { get; set; }
+
         public DateTime? DateOfBirth { get; set; }
+
+        public DateTime? DateOfDeath { get; set; }
+
+        public string Gender { get; set; }
+        public bool DateDateConfirmed { get; set; }
+        public string Incacerated { get; set; }
+
         public IEnumerable<PersonalIdentifier> Identifiers { get; set; }
         public IEnumerable<Address> Addresses { get; set; }
         public IEnumerable<Phone> Phones { get; set; }

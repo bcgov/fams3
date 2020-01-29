85b1d76153ecb02468ff7673849f56db5f33d7f1
PeggyZhangNtt <190728@nttdata.com>
2020-01-29 14:45:25 -0800

    update personfoundservice and personsearchController unit test.

---
+++ app/DynamicsAdapter/DynamicsAdapter.Web.Test/PersonSearch/PersonFoundServiceTest.cs
@@ -0,0 +1,286 @@
+﻿using AutoMapper;
+using DynamicsAdapter.Web.PersonSearch;
+using DynamicsAdapter.Web.PersonSearch.Models;
+using Fams3Adapter.Dynamics.Address;
+using Fams3Adapter.Dynamics.Identifier;
+using Fams3Adapter.Dynamics.Name;
+using Fams3Adapter.Dynamics.Person;
+using Fams3Adapter.Dynamics.PhoneNumber;
+using Fams3Adapter.Dynamics.SearchRequest;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DynamicsAdapter.Web.Test.PersonSearch
+{
+    public class PersonFoundServiceTest
+    {
+
+        private PersonFoundService _sut;
+        private Mock<ILogger<PersonFoundService>> _loggerMock;
+        private Mock<ISearchRequestService> _searchRequestServiceMock;
+        private Person _fakePerson;
+
+        private SSG_Identifier _fakePersoneIdentifier;
+        private SSG_Address _fakePersonAddress;
+        private SSG_PhoneNumber _fakePersonPhoneNumber;
+        private SSG_Aliase _fakeName;
+        private SSG_Person_Upload _ssg_fakePerson;
+        private ProviderProfile _providerProfile;
+        private SSG_SearchRequest _searchRequest;
+        private CancellationToken _fakeToken;
+
+        private Mock<IMapper> _mapper;
+
+        [SetUp]
+        public void Init()
+        {
+
+            _loggerMock = new Mock<ILogger<PersonFoundService>>();
+            _searchRequestServiceMock = new Mock<ISearchRequestService>();
+            _mapper = new Mock<IMapper>();
+            var validRequestId = Guid.NewGuid();
+            var invalidRequestId = Guid.NewGuid();
+
+            _fakePersoneIdentifier = new SSG_Identifier
+            {
+                SearchRequest = new SSG_SearchRequest
+                {
+                    SearchRequestId = validRequestId
+                }
+            };
+
+            _fakePersonAddress = new SSG_Address
+            {
+                SearchRequest = new SSG_SearchRequest
+                {
+                    SearchRequestId = validRequestId
+                }
+            };
+
+            _fakePersonPhoneNumber = new SSG_PhoneNumber
+            {
+                SearchRequest = new SSG_SearchRequest
+                {
+                    SearchRequestId = validRequestId
+                }
+            };
+
+            _fakeName = new SSG_Aliase
+            {
+                SearchRequest = new SSG_SearchRequest
+                {
+                    SearchRequestId = validRequestId
+                }
+            };
+
+            _ssg_fakePerson = new SSG_Person_Upload
+            {
+                
+            };
+
+            _searchRequest = new SSG_SearchRequest
+            {
+                SearchRequestId = validRequestId
+            };
+
+            _fakePerson = new Person()
+            {
+                DateOfBirth = DateTime.Now,
+                FirstName = "TEST1",
+                LastName = "TEST2",
+                Identifiers = new List<PersonalIdentifier>()
+                        {
+                            new PersonalIdentifier()
+                            {
+                               Value  = "test",
+                               IssuedBy = "test",
+                               Type = PersonalIdentifierType.DriverLicense
+                            }
+                        },
+                Addresses = new List<Address>()
+                        {
+                            new Address()
+                            {
+                                AddressLine1 = "AddressLine1",
+                                AddressLine2 = "AddressLine2",
+                                AddressLine3 = "AddressLine3",
+                                StateProvince = "Manitoba",
+                                City = "testCity",
+                                Type = "residence",
+                                CountryRegion= "canada",
+                                ZipPostalCode = "p3p3p3",
+                                ReferenceDates = new List<ReferenceDate>(){
+                                    new ReferenceDate(){ Index=0, Key="Start Date", Value=new DateTime(2019,9,1) },
+                                    new ReferenceDate(){ Index=1, Key="End Date", Value=new DateTime(2020,9,1) }
+                                },
+                                Description = "description"
+                            }
+                        },
+                Phones = new List<Phone>()
+                    {
+                        new Phone ()
+                        {
+                            PhoneNumber = "4005678900"
+                        }
+                    },
+                Names = new List<Name>()
+                    {
+                        new Name ()
+                        {
+                            FirstName = "firstName"
+                        }
+                    }
+
+            };
+
+            _providerProfile = new ProviderProfile()
+            {
+                Name = "TEST PROVIDER"
+            };
+
+            _fakeToken = new CancellationToken();
+
+            _mapper.Setup(m => m.Map<SSG_Identifier>(It.IsAny<PersonalIdentifier>()))
+                               .Returns(_fakePersoneIdentifier);
+
+            _mapper.Setup(m => m.Map<SSG_PhoneNumber>(It.IsAny<Phone>()))
+                             .Returns(_fakePersonPhoneNumber);
+
+            _mapper.Setup(m => m.Map<SSG_Address>(It.IsAny<Address>()))
+                              .Returns(_fakePersonAddress);
+
+            _mapper.Setup(m => m.Map<SSG_Aliase>(It.IsAny<Name>()))
+                  .Returns(_fakeName);
+
+            _mapper.Setup(m => m.Map<SSG_Person_Upload>(It.IsAny<Person>()))
+               .Returns(_ssg_fakePerson);
+
+            _searchRequestServiceMock.Setup(x => x.CreateIdentifier(It.Is<SSG_Identifier>(x => x.SearchRequest.SearchRequestId == invalidRequestId), It.IsAny<CancellationToken>()))
+                .Throws(new Exception("random exception"));
+
+            _searchRequestServiceMock.Setup(x => x.CreateAddress(It.Is<SSG_Address>(x => x.SearchRequest.SearchRequestId == validRequestId), It.IsAny<CancellationToken>()))
+                .Returns(Task.FromResult<SSG_Address>(new SSG_Address()
+                {
+                    AddressLine1 = "test full line"
+                }));
+
+            _searchRequestServiceMock.Setup(x => x.CreatePhoneNumber(It.Is<SSG_PhoneNumber>(x => x.SearchRequest.SearchRequestId == validRequestId), It.IsAny<CancellationToken>()))
+              .Returns(Task.FromResult<SSG_PhoneNumber>(new SSG_PhoneNumber()
+              {
+                  TelePhoneNumber = "4007678231"
+              }));
+
+            _searchRequestServiceMock.Setup(x => x.CreateName(It.Is<SSG_Aliase>(x => x.SearchRequest.SearchRequestId == validRequestId), It.IsAny<CancellationToken>()))
+                .Returns(Task.FromResult<SSG_Aliase>(new SSG_Aliase()
+                {
+                    FirstName = "firstName"
+                }));
+
+            _searchRequestServiceMock.Setup(x => x.SavePerson(It.Is<SSG_Person>(x => x.SearchRequest.SearchRequestId == validRequestId), It.IsAny<CancellationToken>()))
+            .Returns(Task.FromResult<SSG_Person>(new SSG_Person()
+            {
+                FirstName = "First"
+            }));
+
+           _sut = new PersonFoundService(_searchRequestServiceMock.Object, _loggerMock.Object, _mapper.Object);
+
+        }
+
+        [Test]
+        public async Task valid_Person_should_be_processed_correctly()
+        {
+
+            var result = await _sut.ProcessPersonFound(_fakePerson, _providerProfile, _searchRequest, _fakeToken);
+
+            _searchRequestServiceMock
+              .Verify(x => x.SavePerson(It.IsAny<SSG_Person_Upload>(), It.IsAny<CancellationToken>()), Times.Once);
+
+            _searchRequestServiceMock
+                .Verify(x => x.CreateIdentifier(It.IsAny<SSG_Identifier>(), It.IsAny<CancellationToken>()), Times.Once);
+
+            _searchRequestServiceMock
+                 .Verify(x => x.CreateAddress(It.IsAny<SSG_Address>(), It.IsAny<CancellationToken>()), Times.Once);
+
+            _searchRequestServiceMock
+                .Verify(x => x.CreatePhoneNumber(It.IsAny<SSG_PhoneNumber>(), It.IsAny<CancellationToken>()), Times.Once);
+
+            _searchRequestServiceMock
+                .Verify(x => x.CreateName(It.IsAny<SSG_Aliase>(), It.IsAny<CancellationToken>()), Times.Once);
+
+            Assert.AreEqual(true, result);
+        }
+
+        //[Test]
+        //public async Task With_exception_completed_event_should_return_badrequest()
+        //{
+        //    var result = await _sut.Completed(_exceptionGuid, _fakePersonCompletedEvent);
+        //    Assert.IsInstanceOf(typeof(BadRequestResult), result);
+        //}
+
+        //[Test]
+        //public async Task With_valid_accepted_event_it_should_return_ok()
+        //{
+        //    var result = await _sut.Accepted(_testGuid, fakePersonAcceptedEvent);
+        //    _searchApiRequestServiceMock
+        //        .Verify(x => x.AddEventAsync(It.Is<Guid>(x => x == _testGuid), It.IsAny<SSG_SearchApiEvent>(), It.IsAny<CancellationToken>()), Times.Once);
+
+        //    Assert.IsInstanceOf(typeof(OkResult), result);
+        //}
+
+
+        //[Test]
+        //public async Task With_exception_event_it_should_return_badrequest()
+        //{
+        //    var result = await _sut.Accepted(_exceptionGuid, null);
+        //    Assert.IsInstanceOf(typeof(BadRequestResult), result);
+        //}
+
+
+        //[Test]
+        //public async Task With_valid_failed_event_it_should_return_ok()
+        //{
+        //    var result = await _sut.Failed(_testGuid, fakePersonFailedEvent);
+
+
+        //    _searchApiRequestServiceMock
+        //        .Verify(x => x.AddEventAsync(It.Is<Guid>(x => x == _testGuid), It.IsAny<SSG_SearchApiEvent>(), It.IsAny<CancellationToken>()), Times.Once);
+
+        //    Assert.IsInstanceOf(typeof(OkResult), result);
+        //}
+
+
+        //[Test]
+        //public async Task With_exception_failed_event_it_should_return_badrequest()
+        //{
+        //    var result = await _sut.Failed(_exceptionGuid, null);
+        //    Assert.IsInstanceOf(typeof(BadRequestResult), result);
+        //}
+
+        //[Test]
+        //public async Task With_valid_rejected_event_it_should_return_ok()
+        //{
+        //    var result = await _sut.Rejected(_testGuid, fakePersonRejectEvent
+        //        );
+
+
+        //    _searchApiRequestServiceMock
+        //        .Verify(x => x.AddEventAsync(It.Is<Guid>(x => x == _testGuid), It.IsAny<SSG_SearchApiEvent>(), It.IsAny<CancellationToken>()), Times.Once);
+
+        //    Assert.IsInstanceOf(typeof(OkResult), result);
+        //}
+
+
+        //[Test]
+        //public async Task With_exception_rejected_event_it_should_return_badrequest()
+        //{
+        //    var result = await _sut.Rejected(_exceptionGuid, null);
+        //    Assert.IsInstanceOf(typeof(BadRequestResult), result);
+        //}
+    }
+}
--- app/DynamicsAdapter/DynamicsAdapter.Web.Test/PersonSearch/PersonSearchControllerTest.cs
+++ app/DynamicsAdapter/DynamicsAdapter.Web.Test/PersonSearch/PersonSearchControllerTest.cs
@@ -119,49 +119,10 @@ namespace DynamicsAdapter.Web.Test.PersonSearch
                     DateOfBirth = DateTime.Now,
                     FirstName = "TEST1",
                     LastName = "TEST2",
-                    Identifiers = new List<PersonalIdentifier>()
-                        {
-                            new PersonalIdentifier()
-                            {
-                               Value  = "test",
-                               IssuedBy = "test",
-                               Type = PersonalIdentifierType.DriverLicense
-                            }
-                        },
-                    Addresses = new List<Address>()
-                        {
-                            new Address()
-                            {
-                                AddressLine1 = "AddressLine1",
-                                AddressLine2 = "AddressLine2",
-                                AddressLine3 = "AddressLine3",
-                                StateProvince = "Manitoba",
-                                City = "testCity",
-                                Type = "residence",
-                                CountryRegion= "canada",
-                                ZipPostalCode = "p3p3p3",
-                                ReferenceDates = new List<ReferenceDate>(){
-                                    new ReferenceDate(){ Index=0, Key="Start Date", Value=new DateTime(2019,9,1) },
-                                    new ReferenceDate(){ Index=1, Key="End Date", Value=new DateTime(2020,9,1) }
-                                },
-                                Description = "description"
-                            }
-                        },
-                    Phones = new List<Phone>()
-                    {
-                        new Phone ()
-                        {
-                            PhoneNumber = "4005678900"
-                        }
-                    },
-                    Names = new List<Name>()
-                    {
-                        new Name ()
-                        {
-                            FirstName = "firstName"
-                        }
-                    }
-
+                    Identifiers = new List<PersonalIdentifier>() { },                       
+                    Addresses = new List<Address>(){ },
+                    Phones = new List<Phone>(){ },
+                    Names = new List<Name>(){ }
                 }
             };
 
@@ -226,34 +187,6 @@ namespace DynamicsAdapter.Web.Test.PersonSearch
             _personFoundServiceMock.Setup(x => x.ProcessPersonFound(It.Is<Person>(x => x.FirstName == "TEST1"),It.IsAny<ProviderProfile>(), It.IsAny<SSG_SearchRequest>(), It.IsAny<CancellationToken>()))
                 .Returns(Task.FromResult<bool>(true));
 
-            //_searchRequestServiceMock.Setup(x => x.CreateIdentifier(It.Is<SSG_Identifier>(x => x.SearchRequest.SearchRequestId == invalidRequestId), It.IsAny<CancellationToken>()))
-            //    .Throws(new Exception("random exception"));
-
-            //_searchRequestServiceMock.Setup(x => x.CreateAddress(It.Is<SSG_Address>(x => x.SearchRequest.SearchRequestId == validRequestId), It.IsAny<CancellationToken>()))
-            //    .Returns(Task.FromResult<SSG_Address>(new SSG_Address()
-            //    {
-            //        AddressLine1 = "test full line"
-            //    }));
-
-            //_searchRequestServiceMock.Setup(x => x.CreatePhoneNumber(It.Is<SSG_PhoneNumber>(x => x.SearchRequest.SearchRequestId == validRequestId), It.IsAny<CancellationToken>()))
-            //  .Returns(Task.FromResult<SSG_PhoneNumber>(new SSG_PhoneNumber()
-            //  {
-            //      TelePhoneNumber = "4007678231"
-            //  }));
-
-            //_searchRequestServiceMock.Setup(x => x.CreateName(It.Is<SSG_Aliase>(x => x.SearchRequest.SearchRequestId == validRequestId), It.IsAny<CancellationToken>()))
-            //    .Returns(Task.FromResult<SSG_Aliase>(new SSG_Aliase()
-            //    {
-            //        FirstName = "firstName"
-            //    }));
-
-            //_searchRequestServiceMock.Setup(x => x.SavePerson(It.Is<SSG_Person>(x => x.SearchRequest.SearchRequestId == validRequestId), It.IsAny<CancellationToken>()))
-            //.Returns(Task.FromResult<SSG_Person>(new SSG_Person()
-            //{
-            //    FirstName = "First"
-            //}));
-
-
             _searchApiRequestServiceMock.Setup(x => x.AddEventAsync(It.Is<Guid>(x => x == _testGuid),
                     It.IsAny<SSG_SearchApiEvent>(), It.IsAny<CancellationToken>()))
                 .Returns(Task.FromResult<SSG_SearchApiEvent>(new SSG_SearchApiEvent()
@@ -277,25 +210,6 @@ namespace DynamicsAdapter.Web.Test.PersonSearch
             var result = await _sut.Completed(_testGuid, _fakePersonCompletedEvent);
 
             _personFoundServiceMock.Verify(x => x.ProcessPersonFound(It.Is<Person>(x => x.FirstName == "TEST1"), It.IsAny<ProviderProfile>(), It.IsAny<SSG_SearchRequest>(), It.IsAny<CancellationToken>()), Times.Once);
-            //_searchRequestServiceMock
-            //  .Verify(x => x.SavePerson(It.IsAny<SSG_Person>(), It.IsAny<CancellationToken>()), Times.Once);
-
-            //_searchRequestServiceMock
-            //    .Verify(x => x.CreateIdentifier(It.IsAny<SSG_Identifier>(), It.IsAny<CancellationToken>()), Times.Once);
-
-            //_searchRequestServiceMock
-            //     .Verify(x => x.CreateAddress(It.IsAny<SSG_Address>(), It.IsAny<CancellationToken>()), Times.Once);
-
-            //_searchRequestServiceMock
-            //    .Verify(x => x.CreatePhoneNumber(It.IsAny<SSG_PhoneNumber>(), It.IsAny<CancellationToken>()), Times.Once);
-
-            //_searchRequestServiceMock
-            //    .Verify(x => x.CreateName(It.IsAny<SSG_Aliase>(), It.IsAny<CancellationToken>()), Times.Once);
-
-
-            //_searchRequestServiceMock
-            //.Verify(x => x.SavePerson(It.IsAny<SSG_Person>(), It.IsAny<CancellationToken>()), Times.Once);
-
             _searchApiRequestServiceMock
                 .Verify(x => x.AddEventAsync(It.Is<Guid>(x => x == _testGuid), It.IsAny<SSG_SearchApiEvent>(), It.IsAny<CancellationToken>()), Times.Once);
             Assert.IsInstanceOf(typeof(OkResult), result);

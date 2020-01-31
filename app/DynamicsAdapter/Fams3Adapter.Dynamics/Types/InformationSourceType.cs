6e7c30a6681529b527e412d8f9683b37d2dad634
PeggyZhangNtt <190728@nttdata.com>
2020-01-31 11:45:55 -0800

    remove Employer as Dynamics remove it.

--- app/DynamicsAdapter/Fams3Adapter.Dynamics/Types/InformationSourceType.cs
+++ app/DynamicsAdapter/Fams3Adapter.Dynamics/Types/InformationSourceType.cs
@@ -11,7 +11,6 @@ namespace Fams3Adapter.Dynamics.Types
 
         public static InformationSourceType Request = new InformationSourceType(867670000, "Request");
         public static InformationSourceType ICBC = new InformationSourceType(867670001, "ICBC");
-        public static InformationSourceType Employer = new InformationSourceType(867670002,  "Employer");
         public static InformationSourceType Other = new InformationSourceType(867670003, "Other");
 
         protected InformationSourceType(int value, string name) : base(value, name)

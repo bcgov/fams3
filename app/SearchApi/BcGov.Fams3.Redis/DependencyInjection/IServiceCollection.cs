cc9f34e8a5d913ce5b1c09078364b38cb1fe1493
Ranti Familusi <ranti.familusi@nttdata.com>
2020-02-27 12:42:20 -0700

    add service

---
+++ app/SearchApi/BcGov.Fams3.Redis/DependencyInjection/IServiceCollection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BcGov.Fams3.Redis.DependencyInjection
+{
+    public static class IServiceCollectionExtensions
+    {
+
+        public static void AddProvider(this IServiceCollection services, IConfiguration configuration)
+        {
+
+        }
+
+    }
+}
---
+++ app/SearchApi/BcGov.Fams3.Redis/ICacheService.cs
@@ -0,0 +1,12 @@
+﻿using BcGov.Fams3.SearchApi.Contracts.Person;
+using System;
+using System.Threading.Tasks;
+
+namespace BcGov.Fams3.Redis
+{
+    public interface ICacheService
+    {
+        Task SaveRequest(Guid searchRequestId, Person searchRequest);
+        Task<Person> GetRequest(Guid searchRequestId);
+    }
+}

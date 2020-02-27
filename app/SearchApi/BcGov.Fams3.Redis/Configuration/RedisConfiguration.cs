9b3c95675c83a4c13ba8605be2242ef205203c0d
Ranti Familusi <ranti.familusi@nttdata.com>
2020-02-27 12:02:24 -0700

    create project, update docker and add dependency

---
+++ app/SearchApi/BcGov.Fams3.Redis/Configuration/RedisConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BcGov.Fams3.Redis.Configuration
+{
+    /// <summary>
+    /// Represent the Redis configuration
+    /// Settings can be overwriten by environment variables
+    /// </summary>
+    public class RedisConfiguration
+    {
+
+        public RedisConfiguration()
+        {
+            this.Host = "localhost";
+            this.Port = 6379;
+        }
+
+        /// <summary>
+        /// Redis Host
+        /// </summary>
+        public string Host { get; set; }
+
+
+        /// <summary>
+        /// Redis Port
+        /// </summary>
+        public int Port { get; set; }
+    }
+}

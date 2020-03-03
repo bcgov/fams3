3b66a06c7a2d6329f7d16ab653da28135727efae
PeggyZhangNtt <190728@nttdata.com>
2020-03-02 16:30:45 -0800

    update unit test

--- app/SearchApi/SearchApi.Web.Test/People/PeopleControllerTest.cs
+++ app/SearchApi/SearchApi.Web.Test/People/PeopleControllerTest.cs
@@ -29,13 +29,15 @@ namespace SearchApi.Web.Test.People
         private readonly Mock<ICacheService> _cacheMock = new Mock<ICacheService>();
 
         private Mock<IBusControl> _busControlMock;
+        private Guid expectedId = Guid.NewGuid();
 
-            [SetUp]
+        [SetUp]
         public void Init()
         {
             _busControlMock = new Mock<IBusControl>();
             _spanMock.Setup(x => x.SetTag(It.IsAny<string>(), It.IsAny<string>())).Returns(_spanMock.Object);
             _tracerMock.Setup(x => x.ActiveSpan).Returns(_spanMock.Object);
+            
             _sut = new PeopleController(_busControlMock.Object, _loggerMock.Object, _tracerMock.Object, _cacheMock.Object);
         }
 
@@ -48,7 +50,7 @@ namespace SearchApi.Web.Test.People
 
             Assert.IsInstanceOf<PersonSearchResponse>(result.Value); 
             Assert.IsNotNull(((PersonSearchResponse)result.Value).Id);
-            _cacheMock.Verify(x => x.SaveRequest(It.IsAny<SearchRequest>()), Times.Once);
+            _cacheMock.Verify(x => x.SaveRequest(It.Is<SearchRequest>(m => m.SearchRequestId != expectedId)), Times.Once);
             _spanMock.Verify(x => x.SetTag("searchRequestId", $"{((PersonSearchResponse)result.Value).Id}"), Times.Once);
             _busControlMock.Verify(x => x.Publish(It.IsAny<PersonSearchOrdered>(), It.IsAny<CancellationToken>()), Times.Once);
         }
@@ -56,15 +58,13 @@ namespace SearchApi.Web.Test.People
         [Test]
         public void With_valid_payload_and_id_should_return_created()
         {
-            var expectedId = Guid.NewGuid();
-
             var result =
                 (AcceptedResult)this._sut.Search($"{expectedId}", new PersonSearchRequest("firstName", "lastName", null, new List<PersonalIdentifier>(), new List<Address>(), new List<Phone>(), new List<Name>(), new List<RelatedPerson>(), new List<Employment>())).Result;
 
             Assert.IsInstanceOf<PersonSearchResponse>(result.Value);
             Assert.AreEqual( expectedId, ((PersonSearchResponse)result.Value).Id);
             _spanMock.Verify(x => x.SetTag("searchRequestId", $"{((PersonSearchResponse)result.Value).Id}"), Times.Once);
-            _cacheMock.Verify(x => x.SaveRequest(It.IsAny<SearchRequest>()), Times.Once);
+            _cacheMock.Verify(x => x.SaveRequest(It.Is<SearchRequest>(m=>m.SearchRequestId== expectedId)), Times.Once);
             _busControlMock.Verify(x => x.Publish(It.IsAny<PersonSearchOrdered>(), It.IsAny<CancellationToken>()), Times.Once);
         }
 

b41dabd4870ab7fc5bef14bed351cc0d0d1f47a7
Babaranti Familusi <ranti.familusi@nttdata.com>
2020-03-04 08:56:24 -0700

    refactor to trigger github action

--- app/SearchApi/BcGov.Fams3.Redis/DependencyInjection/IServiceCollectionExtensions.cs
+++ app/SearchApi/BcGov.Fams3.Redis/DependencyInjection/IServiceCollectionExtensions.cs
@@ -11,19 +11,13 @@ namespace BcGov.Fams3.Redis.DependencyInjection
         public static void AddCacheService(this IServiceCollection services, IConfiguration configuration)
         {
 
-            RedisConfiguration redisSettings = configuration.GetSection("Redis").Get<RedisConfiguration>();
-            if (redisSettings == null)
-            {
-                throw new Exception("Invalid redis settings. ");
-            }
+            var redisSettings = configuration.GetSection("Redis").Get<RedisConfiguration>();
+            if (redisSettings == null) throw new Exception("Invalid redis settings. ");
 
             try
             {
-                if (redisSettings != null)
-                {
-                    services.AddSingleton<IRedisConnectionFactory, RedisConnectionFactory>(factory => {return new RedisConnectionFactory(redisSettings.ConnectionString); });
-                    services.AddSingleton<ICacheService, CacheService>(service => { return new CacheService(service.GetService<IRedisConnectionFactory>()); });
-                }
+                services.AddSingleton<IRedisConnectionFactory, RedisConnectionFactory>(factory => new RedisConnectionFactory(redisSettings.ConnectionString));
+                services.AddSingleton<ICacheService, CacheService>(service => new CacheService(service.GetService<IRedisConnectionFactory>()));
             }
             catch(RedisException e)
             {

79dbacfdb9365e82946f87260158ff1331ab26ba
PeggyZhangNtt <190728@nttdata.com>
2020-03-23 11:57:57 -0700

    update contract-person

--- app/SearchApi/BcGov.Fams3.SearchApi.Contracts/Person/Person.cs
+++ app/SearchApi/BcGov.Fams3.SearchApi.Contracts/Person/Person.cs
@@ -17,13 +17,21 @@ namespace BcGov.Fams3.SearchApi.Contracts.Person
         public string Gender { get; set; }
         public bool? DateDeathConfirmed { get; set; }
         public string Incacerated { get; set; }
+        public string HeightInCms { get; set; }
+        public string WeightInKgs { get; set; }
+        public string HairColour { get; set; }
+        public string EyeColour { get; set; }
+        public string Complexion { get; set; }
+        public string DistinguishingFeatures { get; set; }
+        public string GlassesFlag { get; set; }
+        public string CautionFlag { get; set; }
+        public string SecurityKeyword { get; set; }
 
         public IEnumerable<PersonalIdentifier> Identifiers { get; set; }
         public IEnumerable<Address> Addresses { get; set; }
         public IEnumerable<Phone> Phones { get; set; }
         public IEnumerable<Name> Names { get; set; }
         public IEnumerable<RelatedPerson> RelatedPersons { get; set; }
-
         public IEnumerable<Employment> Employments { get; set; }
 
         public string Notes { get; set; }

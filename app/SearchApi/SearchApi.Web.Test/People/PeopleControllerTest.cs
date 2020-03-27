84da6b6584da7b783f37e2ecea1e0050d55a9efc
alex <alex.joyeux@gov.bc.ca>
2020-03-27 10:51:09 -0700

    fixed test

--- app/SearchApi/SearchApi.Web.Test/People/PeopleControllerTest.cs
+++ app/SearchApi/SearchApi.Web.Test/People/PeopleControllerTest.cs
@@ -22,21 +22,32 @@ namespace SearchApi.Web.Test.People
 
         private PeopleController _sut;
 
-        private readonly Mock<ITracer> _tracerMock = new Mock<ITracer>();
+        private Mock<ITracer> _tracerMock;
 
-        private readonly Mock<ISpan> _spanMock = new Mock<ISpan>();
+        private Mock<ISpan> _spanMock;
 
-        private readonly Mock<ILogger<PeopleController>> _loggerMock = new Mock<ILogger<PeopleController>>();
+        private Mock<ILogger<PeopleController>> _loggerMock;
 
-        private readonly Mock<ICacheService> _cacheMock = new Mock<ICacheService>();
+        private Mock<ICacheService> _cacheMock;
 
-        private readonly Mock<IDispatcher> _dispatcherMock = new Mock<IDispatcher>();
+        private Mock<IDispatcher> _dispatcherMock;
 
         private Guid expectedId = Guid.NewGuid();
 
         [SetUp]
         public void Init()
         {
+
+            _dispatcherMock = new Mock<IDispatcher>();
+
+            _tracerMock = new Mock<ITracer>();
+
+            _spanMock = new Mock<ISpan>();
+
+            _loggerMock = new Mock<ILogger<PeopleController>>();
+
+            _cacheMock = new Mock<ICacheService>();
+
             _spanMock.Setup(x => x.SetTag(It.IsAny<string>(), It.IsAny<string>())).Returns(_spanMock.Object);
             _tracerMock.Setup(x => x.ActiveSpan).Returns(_spanMock.Object);
 
@@ -51,9 +62,9 @@ namespace SearchApi.Web.Test.People
         public void With_valid_payload_should_return_created()
         {
             var result =
-                (AcceptedResult) this._sut.Search(null, new PersonSearchRequest("firstName", "lastName", null, new List<PersonalIdentifier>(), new List<Address>(), new List<Phone>(), new List<Name>(), new List<RelatedPerson>(), new List<Employment>(), new List<DataProvider>())).Result;
+                (AcceptedResult)this._sut.Search(null, new PersonSearchRequest("firstName", "lastName", null, new List<PersonalIdentifier>(), new List<Address>(), new List<Phone>(), new List<Name>(), new List<RelatedPerson>(), new List<Employment>(), new List<DataProvider>())).Result;
 
-            Assert.IsInstanceOf<PersonSearchResponse>(result.Value); 
+            Assert.IsInstanceOf<PersonSearchResponse>(result.Value);
             Assert.IsNotNull(((PersonSearchResponse)result.Value).Id);
             _cacheMock.Verify(x => x.SaveRequest(It.Is<SearchRequest>(m => m.SearchRequestId != expectedId)), Times.Once);
             _spanMock.Verify(x => x.SetTag("searchRequestId", $"{((PersonSearchResponse)result.Value).Id}"), Times.Once);
@@ -67,9 +78,9 @@ namespace SearchApi.Web.Test.People
                 (AcceptedResult)this._sut.Search($"{expectedId}", new PersonSearchRequest("firstName", "lastName", null, new List<PersonalIdentifier>(), new List<Address>(), new List<Phone>(), new List<Name>(), new List<RelatedPerson>(), new List<Employment>(), new List<DataProvider>())).Result;
 
             Assert.IsInstanceOf<PersonSearchResponse>(result.Value);
-            Assert.AreEqual( expectedId, ((PersonSearchResponse)result.Value).Id);
+            Assert.AreEqual(expectedId, ((PersonSearchResponse)result.Value).Id);
             _spanMock.Verify(x => x.SetTag("searchRequestId", $"{((PersonSearchResponse)result.Value).Id}"), Times.Once);
-            _cacheMock.Verify(x => x.SaveRequest(It.Is<SearchRequest>(m=>m.SearchRequestId== expectedId)), Times.Once);
+            _cacheMock.Verify(x => x.SaveRequest(It.Is<SearchRequest>(m => m.SearchRequestId == expectedId)), Times.Once);
             _dispatcherMock.Verify(x => x.Dispatch(It.IsAny<PersonSearchRequest>(), It.IsAny<Guid>()), Times.Once);
         }
 

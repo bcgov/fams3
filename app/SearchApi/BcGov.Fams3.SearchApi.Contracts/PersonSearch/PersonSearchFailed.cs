7dc366f410bbad7e0fc655705ae6f4398c7002b4
PeggyZhangNtt <190728@nttdata.com>
2020-04-07 10:51:31 -0700

    restore failed

--- app/SearchApi/BcGov.Fams3.SearchApi.Contracts/PersonSearch/PersonSearchFailed.cs
+++ app/SearchApi/BcGov.Fams3.SearchApi.Contracts/PersonSearch/PersonSearchFailed.cs
@@ -4,6 +4,5 @@ namespace BcGov.Fams3.SearchApi.Contracts.PersonSearch
     public interface PersonSearchFailed : PersonSearchAdapterEvent
     {
         string Cause { get; }
-        BcGov.Fams3.SearchApi.Contracts.Person.PersonalIdentifier personalIdentifier { get; }
     }
 }

dc7673a2310b506ff266995e92773db6532c3619
PeggyZhangNtt <190728@nttdata.com>
2020-04-07 10:26:27 -0700

    update completed and failed event contract

--- app/SearchApi/BcGov.Fams3.SearchApi.Contracts/PersonSearch/PersonSearchFailed.cs
+++ app/SearchApi/BcGov.Fams3.SearchApi.Contracts/PersonSearch/PersonSearchFailed.cs
@@ -1,7 +1,9 @@
-﻿namespace BcGov.Fams3.SearchApi.Contracts.PersonSearch
+﻿
+namespace BcGov.Fams3.SearchApi.Contracts.PersonSearch
 {
     public interface PersonSearchFailed : PersonSearchAdapterEvent
     {
         string Cause { get; }
+        BcGov.Fams3.SearchApi.Contracts.Person.PersonalIdentifier personalIdentifier { get; }
     }
 }

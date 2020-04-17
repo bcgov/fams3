c11efdfa848215e23c82ec8a80987675db772344
Ranti Familusi <ranti.familusi@nttdata.com>
2020-04-17 13:26:54 -0600

    Auto stash before rebase of "origin/deploy/mshd-configs"
    unit test and code update

--- app/DynamicsAdapter/Fams3Adapter.Dynamics/Types/InformationSourceType.cs
+++ app/DynamicsAdapter/Fams3Adapter.Dynamics/Types/InformationSourceType.cs
@@ -1,5 +1,7 @@
 ﻿using Fams3Adapter.Dynamics.OptionSets.Models;
 using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 
 namespace Fams3Adapter.Dynamics.Types
@@ -13,10 +15,20 @@ namespace Fams3Adapter.Dynamics.Types
         public static InformationSourceType ICBC = new InformationSourceType(867670001, "ICBC");
         public static InformationSourceType Other = new InformationSourceType(867670003, "Other");
         public static InformationSourceType BCHydro = new InformationSourceType(867670005, "BCHydro");
+        public static InformationSourceType MSDPR = new InformationSourceType(867670023, "MSDPR");
 
         protected InformationSourceType(int value, string name) : base(value, name)
         {
         }
     }
    
+    public class ProviderProfileMap
+    {
+        public static readonly ReadOnlyDictionary<string, string> ProvideCodeToProfileNameMap = new ReadOnlyDictionary<string, string>(new Dictionary<string, string>()
+        {
+            {"ICBC", "ICBC"},
+            {"BCHydro", "BCHydro"},
+             { "MHSD","MSDPR" }
+        });
+    }
 }

a6a0c3d6075f27287a8d851cda8f29a9b187b812
PeggyZhangNtt <190728@nttdata.com>
2020-05-08 16:17:38 -0700

    update contract

---
+++ app/SearchApi/BcGov.Fams3.SearchApi.Contracts/Person/Vehicle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace BcGov.Fams3.SearchApi.Contracts.Person
+{
+    public class Vehicle : PersonalInfo
+    {
+        [Description("OwnershipType")]
+        public string OwnershipType { get; set; }
+
+        [Description("Vin")]
+        public string Vin { get; set; }
+
+        [Description("PlateNumber")]
+        public string PlateNumber { get; set; }
+
+        [Description("Owners")]
+        public IEnumerable<AssetOwner> Owners { get; set; }
+    }
+
+}

d21577ccf0777a4ea9e269c33808e44f27b14de0
PeggyZhangNtt <190728@nttdata.com>
2020-05-11 11:43:56 -0700

    upload completed event person info to dynamics

---
+++ app/DynamicsAdapter/Fams3Adapter.Dynamics/AssetOwner/SSG_AssetOwner.cs
@@ -0,0 +1,38 @@
+﻿using Fams3Adapter.Dynamics.Vehicle;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fams3Adapter.Dynamics.AssetOwner
+{
+    public class SSG_AssetOwner
+    {
+        [JsonProperty("ssg_type")]
+        public string Type { get; set; }
+
+        [JsonProperty("ssg_description")]
+        public string Description { get; set; }
+
+        [JsonProperty("ssg_organizationname")]
+        public string OrganizationName { get; set; }
+
+        [JsonProperty("ssg_lastname")]
+        public string LastName { get; set; }
+
+        [JsonProperty("ssg_firstname")]
+        public string FirstName { get; set; }
+
+        [JsonProperty("ssg_middlename")]
+        public string MiddleName { get; set; }
+
+        [JsonProperty("ssg_thirdgivenname")]
+        public string OtherName { get; set; }
+
+        [JsonProperty("ssg_notes")]
+        public string Notes { get; set; }
+
+        [JsonProperty("ssg_Vehicle")]
+        public SSG_Asset_Vehicle Vehicle { get; set; }
+    }
+}
---
+++ app/DynamicsAdapter/Fams3Adapter.Dynamics/Vehicle/SSG_Asset_Vehicle.cs
@@ -0,0 +1,42 @@
+﻿using Fams3Adapter.Dynamics.Person;
+using Fams3Adapter.Dynamics.SearchRequest;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fams3Adapter.Dynamics.Vehicle
+{
+    public class VehicleEntity : DynamicsEntity
+    {
+        [JsonProperty("ssg_ownershiptype")]
+        public string OwnershipType { get; set; }
+
+        [JsonProperty("ssg_vin")]
+        public string Vin { get; set; }
+
+        [JsonProperty("ssg_licenseplate")]
+        public string PlateNumber { get; set; }
+
+        [JsonProperty("ssg_name")]
+        public string Discription { get; set; }
+
+        [JsonProperty("ssg_notes")]
+        public string Notes { get; set; }
+
+        [JsonProperty("ssg_suppliedby")]
+        public int? InformationSource { get; set; }
+
+        [JsonProperty("ssg_SearchRequest")]
+        public virtual SSG_SearchRequest SearchRequest { get; set; }
+
+        [JsonProperty("ssg_PersonId")]
+        public virtual SSG_Person Person { get; set; }
+    }
+
+    public class SSG_Asset_Vehicle : VehicleEntity
+    {
+        [JsonProperty("ssg_asset_vehicleid")]
+        public Guid VehicleId { get; set; }
+    }
+}

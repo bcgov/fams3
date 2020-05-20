2f62393fb69e578c957ad4d54dd3a9f8ed146b81
Ranti Familusi <ranti.familusi@nttdata.com>
2020-05-20 09:39:19 -0600

    insurance contract

---
+++ app/SearchApi/BcGov.Fams3.SearchApi.Contracts/Person/InsuranceClaim.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace BcGov.Fams3.SearchApi.Contracts.Person
+{
+    public  class InsuranceClaim : PersonalInfo
+    {
+
+        [Description("Claim Type")]
+        public string ClaimType { get; set; }
+
+        [Description("Claim Status")]
+        public string ClaimStatus { get; set; }
+
+        [Description("claim number")]
+        public string ClaimNumber { get; set; }
+
+        [Description("claim center")]
+        public string ClaimCenter { get; set; }
+
+        [Description("Adjustor")]
+        public Name Adjustor { get; set; }
+
+        [Description("Adjustor Phone")]
+        public Phone AdjustorPhone { get; set; }
+
+        [Description("Identifer involved in insurance claim")]
+        public IEnumerable<PersonalIdentifier> Identifiers { get; set; }
+
+        [Description("Person involved")]
+        public Name InsuredPerson { get; set; }
+
+
+
+    }
+}

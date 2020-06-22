007971eb432b2382e152d7d7fb261266ae0950e2
PeggyZhangNtt <190728@nttdata.com>
2020-06-22 08:52:42 -0700

    update recieved event to use string as payload.

--- app/SearchApi/BcGov.Fams3.SearchApi.Contracts/PersonSearch/PersonSearchReceived.cs
+++ app/SearchApi/BcGov.Fams3.SearchApi.Contracts/PersonSearch/PersonSearchReceived.cs
@@ -6,6 +6,6 @@ namespace BcGov.Fams3.SearchApi.Contracts.PersonSearch
 {
    public interface PersonSearchReceived : PersonSearchEvent
     {
-        object ReceivedPayload { get; }
+        string ReceivedPayload { get; }
     }
 }

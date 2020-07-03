172eefde1229f3f1baca857c2b7e1c757869406c
Ranti Familusi <ranti.familusi@nttdata.com>
2020-07-03 09:01:43 -0600

    add type

---
+++ app/DynamicsAdapter/Fams3Adapter.Dynamics/Types/IncomeAssistanceStatusType.cs
@@ -0,0 +1,14 @@
+﻿using Fams3Adapter.Dynamics.OptionSets.Models;
+
+namespace Fams3Adapter.Dynamics.Types
+{
+    public class IncomeAssistanceStatusType : Enumeration
+    {
+        public static IncomeAssistanceStatusType Active = new IncomeAssistanceStatusType(867670000, "Active");
+        public static IncomeAssistanceStatusType Closed = new IncomeAssistanceStatusType(867670001, "Closed");
+
+        protected IncomeAssistanceStatusType(int value, string name) : base(value, name)
+        {
+        }
+    }
+}

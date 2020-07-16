05e1faa64164cbcea9d82493d1887b16386be162
Ranti Familusi <ranti.familusi@nttdata.com>
2020-07-16 06:58:41 -0600

    refactor and add sample.

---
+++ app/SearchApi/AgencAdapter.Sample.Test/SearchRequestPublisherTest.cs
@@ -0,0 +1,71 @@
+﻿using AgencyAdapter.Sample.SearchRequest;
+using FluentValidation;
+using FluentValidation.Results;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AgencAdapter.Sample.Test
+{
+    public class SearchRequestPublisherTest
+    {
+        private SearchRequestPublisher _sut;
+        Mock<IValidator<AgencyAdapter.Sample.Models.SearchRequest>> validatorMock;
+        Mock<ILogger<SearchRequestPublisher>> loggerMock;
+        AgencyAdapter.Sample.Models.SearchRequest requestValid;
+        AgencyAdapter.Sample.Models.SearchRequest requestInValid;
+
+        [SetUp]
+        public void SetUp()
+        {
+            validatorMock = new Mock<IValidator<AgencyAdapter.Sample.Models.SearchRequest>>();
+            loggerMock = new Mock<ILogger<SearchRequestPublisher>>();
+
+
+            validatorMock.Setup(x => x.Validate(It.Is<AgencyAdapter.Sample.Models.SearchRequest>(request => !string.IsNullOrEmpty(request.RequestorAgencyCode))))
+        .Returns(new ValidationResult(Enumerable.Empty<ValidationFailure>()));
+
+
+            validatorMock.Setup(x => x.Validate(It.Is<AgencyAdapter.Sample.Models.SearchRequest>(request => string.IsNullOrEmpty(request.RequestorAgencyCode))))
+           .Returns(new ValidationResult(new List<ValidationFailure>()
+               {
+                    new ValidationFailure("RequestorAgencyCode", "Code is required.")
+               }));
+
+            requestValid = new AgencyAdapter.Sample.Models.SearchRequest
+            {
+                RequestorAgencyCode = "Code"
+            };
+            requestInValid = new AgencyAdapter.Sample.Models.SearchRequest
+            {
+                RequestorAgencyCode = ""
+            };
+            _sut = new SearchRequestPublisher(validatorMock.Object, loggerMock.Object);
+        }
+
+        [Test]
+        public async Task  when_request_valid_should_publish()
+        {
+           await  _sut.ProcessRequest(requestValid);
+
+            loggerMock.VerifyLog(LogLevel.Debug, "Start the process", Times.Once());
+            loggerMock.VerifyLog(LogLevel.Debug, "Attempting to validate the Search Request", Times.Once());
+            loggerMock.VerifyLog(LogLevel.Debug, "Request is valid.", Times.Once());
+            loggerMock.VerifyLog(LogLevel.Debug, "Process and send to queue", Times.Once());
+        }
+
+        [Test]
+        public async Task when_request_invalid_should_not_publish()
+        {
+            await _sut.ProcessRequest(requestInValid);
+
+            loggerMock.VerifyLog(LogLevel.Debug, "Start the process", Times.Once());
+            loggerMock.VerifyLog(LogLevel.Debug, "Attempting to validate the Search Request", Times.Once());
+            loggerMock.VerifyLog(LogLevel.Debug, "Request failed.", Times.Once());
+            loggerMock.VerifyLog(LogLevel.Debug, "Process and send to queue", Times.Never());
+        }
+    }
+}
---
+++ app/SearchApi/AgencAdapter.Sample.Test/SearchRequestResponseConsumerTest.cs
@@ -0,0 +1,81 @@
+﻿using AgencyAdapter.Sample.SearchRequest;
+using BcGov.Fams3.SearchApi.Contracts.Person;
+using BcGov.Fams3.SearchApi.Contracts.PersonSearch;
+using BcGov.Fams3.SearchApi.Contracts.SearchRequest;
+using BcGov.Fams3.SearchApi.Core.Adapters.Configuration;
+using MassTransit.Testing;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AgencAdapter.Sample.Test
+{
+    public class SearchRequestResponseConsumerTest
+    {
+
+        private InMemoryTestHarness _harness;
+        private ConsumerTestHarness<SearchRequestResponseConsumer> _sut;
+
+        private Mock<ILogger<SearchRequestResponseConsumer>> _loggerMock;
+       
+      
+
+        private Guid validGuid = Guid.NewGuid();
+ 
+
+    
+
+        [OneTimeSetUp]
+        public async Task A_consumer_is_being_tested()
+        {
+
+            _loggerMock = new Mock<ILogger<SearchRequestResponseConsumer>>();
+           
+            
+
+
+            _harness = new InMemoryTestHarness();
+            _sut = _harness.Consumer(() => new SearchRequestResponseConsumer( _loggerMock.Object));
+
+            await _harness.Start();
+
+            await _harness.BusControl.Publish<SearchRequestNotification>(new SearchRequestNotificationSample()
+            {
+                ProviderProfile = new AgencySample { Name = "FEMP" },
+                SearchRequestId = validGuid,
+                SearchRequestKey = "SearchRequestKey",
+                RequestId = "RequestId",
+                TimeStamp = DateTime.Now,
+
+            });
+
+         
+
+        }
+
+
+        [OneTimeTearDown]
+        public async Task Teardown()
+        {
+            await _harness.Stop();
+        }
+
+        [Test]
+        public void Should_send_the_initial_message_to_the_consumer()
+        {
+            Assert.IsTrue(_harness.Published.Select<SearchRequestNotification>().Any());
+        }
+
+        [Test]
+        public void Should_receive_the_message_type_publish_recieved()
+        {
+            Assert.IsTrue(_harness.Consumed.Select<SearchRequestNotification>().Any());
+        }
+
+        
+    }
+}

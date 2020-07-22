6d3d967e8cd66d79b12693025cd62e0a68a8d146
PeggyZhangNtt <190728@nttdata.com>
2020-07-22 16:28:13 -0700

    add agencyRequestController

---
+++ app/DynamicsAdapter/DynamicsAdapter.Web/SearchAgency/AgencyRequestController.cs
@@ -0,0 +1,69 @@
+﻿using AutoMapper;
+using DynamicsAdapter.Web.PersonSearch;
+using DynamicsAdapter.Web.SearchAgency.Models;
+using Fams3Adapter.Dynamics.SearchApiRequest;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NSwag.Annotations;
+using System.Threading.Tasks;
+
+namespace DynamicsAdapter.Web.SearchAgency
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AgencyRequestController : ControllerBase
+    {
+        private readonly ILogger<PersonSearchController> _logger;
+        private readonly ISearchApiRequestService _searchApiRequestService;
+        private readonly IMapper _mapper;
+
+        public AgencyRequestController(
+                ISearchApiRequestService searchApiRequestService,
+                ILogger<PersonSearchController> logger,
+                IMapper mapper
+                )
+        {
+
+            _searchApiRequestService = searchApiRequestService;
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [Route("CreateSearchRequest/{key}")]
+        [OpenApiTag("Agency Search Reqeust API")]
+        public async Task<IActionResult> CreateSearchRequest(string key, [FromBody]SearchRequestOrdered personCompletedEvent)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [Route("UpdateSearchRequest/{key}")]
+        [OpenApiTag("Agency Search Reqeust API")]
+        public async Task<IActionResult> UpdateSearchRequest(string key, [FromBody]SearchRequestOrdered personCompletedEvent)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [Route("CancelSearchRequest/{key}")]
+        [OpenApiTag("Agency Search Reqeust API")]
+        public async Task<IActionResult> CancelSearchRequest(string key, [FromBody]SearchRequestOrdered personCompletedEvent)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ app/DynamicsAdapter/DynamicsAdapter.Web/SearchAgency/Models/SearchRequestEvent.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DynamicsAdapter.Web.SearchAgency.Models
+{
+    public class SearchRequestEvent
+    {
+        /// <summary>
+        /// This is Data Partner Id for the SearchRequest
+        /// </summary>
+        string RequestId { get;  }
+        /// <summary>
+        /// This is the Key to identify the searchRequest in dynamics it is made of FileId_SequenceNumber
+        /// </summary>
+        string SearchRequestKey { get; }
+        /// <summary>
+        /// This is Guid of the search request
+        /// </summary>
+        Guid SearchRequestId { get; }
+        DateTime TimeStamp { get; }
+        ////
+        /// <summary>
+        ///   action requested by agency, new, update, cancel, notify"
+        /// </summary>
+        public RequestAction Action { get; set; }
+    }
+
+    public enum RequestAction
+    {
+        NEW,
+        UPDATE,
+        CANCEL,
+        NOTIFY
+    }
+}

9c7d21ce0ecf624d48942fd160f1d6ad1f9de3db
PeggyZhangNtt <190728@nttdata.com>
2020-07-22 14:27:20 -0700

    add unit tests for consumer and options

---
+++ app/SearchApi/SearchRequest.Adaptor.Test/Configuration/SearchRequestAdaptorOptionsTest.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using SearchRequestAdaptor.Configuration;
+using System.Linq;
+
+namespace SearchRequest.Adaptor.Test.Configuration
+{
+    public class SearchRequestAdaptorOptionsTest
+    {
+        [Test]
+        public void Should_add_a_new_webHook()
+        {
+
+            var options = new SearchRequestAdaptorOptions().AddWebHook("test", "http://example.com/post");
+
+            Assert.AreEqual(1, options.WebHooks.Count);
+            Assert.AreEqual("test", options.WebHooks.FirstOrDefault().Name);
+            Assert.AreEqual("http://example.com/post", options.WebHooks.FirstOrDefault().Uri);
+
+        }
+
+        [Test]
+        public void Should_add_a_new_webHook_even_name_is_null()
+        {
+            var options = new SearchRequestAdaptorOptions().AddWebHook(null, "http://example.com/post");
+
+            Assert.AreEqual(1, options.WebHooks.Count);
+            Assert.AreEqual(null, options.WebHooks.FirstOrDefault().Name);
+            Assert.AreEqual("http://example.com/post", options.WebHooks.FirstOrDefault().Uri);
+
+        }
+    }
+}

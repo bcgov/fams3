a2a4c5089d6ae077d6bb34ffcb869fbecc55bcea
Ranti Familusi <ranti.familusi@nttdata.com>
2020-08-11 10:03:08 -0600

    add controller

---
+++ app/SearchApi/SearchRequest.Adaptor/Notifier/NotificationController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SearchRequest.Adaptor.Notifier
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+        public NotificationController()
+        {
+
+        }
+    }
+}

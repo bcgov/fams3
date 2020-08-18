d9c745aac7d45c90372a4bf65a8e69d197a6aa6c
PeggyZhangNtt <190728@nttdata.com>
2020-08-17 17:20:44 -0700

    add updateObject proj

---
+++ app/SearchApi/BcGov.Fams3.Utils.Test/IUpdatableObjectTest.cs
@@ -0,0 +1,111 @@
+using BcGov.Fams3.Utils.Object;
+using NUnit.Framework;
+using System;
+
+namespace BcGov.Fams3.Utils.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void same_data_updated_should_be_false()
+        {
+            Guid guid = Guid.NewGuid();
+            TestUpdatableCls testUpdatableCls1 = new TestUpdatableCls()
+            {
+                PropDateTime = new DateTime(1999, 1, 1),
+                PropInt = 2,
+                PropString = "test1",
+                PropNullableDateTime = null,
+                PropNullableInt = null,
+                PropGuid = guid,
+                Child = new ChildTestUpdatableCls()
+                {
+                    PropInt = 4
+                }
+            };
+            TestUpdatableCls testUpdatableCls2 = testUpdatableCls1.DeepClone();
+            TestUpdatableCls result = testUpdatableCls1.MergeUpdates(testUpdatableCls2);
+            Assert.AreEqual(false, result.Updated);
+            Assert.AreEqual(false, result.Child.Updated);
+        }
+
+        [Test]
+        public void null_data_no_updated_should_be_false()
+        {
+            Guid guid = Guid.NewGuid();
+            TestUpdatableCls testUpdatableCls1 = new TestUpdatableCls()
+            {
+                PropNullableDateTime = new DateTime(2000,1,1),
+            };
+            TestUpdatableCls testUpdatableCls2 = new TestUpdatableCls()
+            {
+                PropNullableDateTime = null,
+
+            };
+            TestUpdatableCls result = testUpdatableCls1.MergeUpdates(testUpdatableCls2);
+            Assert.AreEqual(false, result.Updated);
+        }
+
+        [Test]
+        public void different_data_updated_should_be_true()
+        {
+            TestUpdatableCls testUpdatableCls1 = new TestUpdatableCls()
+            {
+                PropDateTime = new DateTime(1999, 1, 1),
+                PropInt = 2,
+                PropString = "test1",
+                PropNullableDateTime = new DateTime(2000,1,1),
+                PropNullableInt = null,
+                Child = new ChildTestUpdatableCls()
+                {
+                    PropInt = 3
+                }
+            };
+            TestUpdatableCls testUpdatableCls2 = new TestUpdatableCls()
+            {
+                PropDateTime = new DateTime(1999, 1, 1),
+                PropInt = 2,
+                PropString = "test1",
+                PropNullableDateTime = null,
+                PropNullableInt = 3,
+                Child = new ChildTestUpdatableCls()
+                {
+                    PropInt = 4
+                }
+            };
+            TestUpdatableCls result = testUpdatableCls1.MergeUpdates(testUpdatableCls2);
+            Assert.AreEqual(true, result.Updated);
+            Assert.AreEqual(new DateTime(2000, 1, 1), result.PropNullableDateTime);
+            Assert.AreEqual(3, result.PropNullableInt);
+            Assert.AreEqual(true, result.Child.Updated);
+            Assert.AreEqual(4, result.Child.PropInt);
+        }
+    }
+
+    public class TestUpdatableCls : IUpdatableObject
+    { 
+        public int PropInt { get; set; }
+        public int? PropNullableInt { get; set; }
+        public string PropString { get; set; }
+        public DateTime PropDateTime { get; set; }
+        public DateTime? PropNullableDateTime { get; set; }
+        public bool Updated { get; set; }
+        public Guid PropGuid { get; set; }
+        public ChildTestUpdatableCls Child { get; set; }
+    }
+
+    public class ChildTestUpdatableCls : IUpdatableObject
+    {
+        public int PropInt { get; set; }
+        public int? PropNullableInt { get; set; }
+        public string PropString { get; set; }
+        public DateTime PropDateTime { get; set; }
+        public DateTime? PropNullableDateTime { get; set; }
+        public bool Updated { get; set; }
+    }
+}
---
+++ app/SearchApi/BcGov.Fams3.Utils/Object/IUpdatableObject.cs
@@ -0,0 +1,7 @@
+﻿namespace BcGov.Fams3.Utils.Object
+{
+    public interface IUpdatableObject
+    {
+        public bool Updated { get; set; }
+    }
+}

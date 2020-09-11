da16ad6c62e0c793a9aaf43b0506a60aa74ba46a
PeggyZhangNtt <190728@nttdata.com>
2020-09-11 15:03:16 -0700

    add submitToQueue action

--- app/DynamicsAdapter/DynamicsAdapter.Web/ApiGateway/ApiGatewayHandler.cs
+++ app/DynamicsAdapter/DynamicsAdapter.Web/ApiGateway/ApiGatewayHandler.cs
@@ -2,6 +2,8 @@
 using Microsoft.Extensions.Options;
 using System;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -27,6 +29,17 @@ namespace DynamicsAdapter.Web.ApiGateway
             if (Uri.TryCreate(CombineUrls(_apiGatewayOptions.BasePath, request.RequestUri.PathAndQuery), UriKind.Absolute, out var path))
             {
                 request.Headers.Add("MSCRM.SuppressDuplicateDetection", "false");
+
+                //this is to deal with Dynamics, when the method is POST and payload is empty, 
+                //Dynamics still looking for content-type.
+                //this senario now only happens with ssg_SearchRequestSubmittoQueueActions
+                if (request.Content == null
+                    && request.Method == HttpMethod.Post
+                    && request.RequestUri.AbsolutePath.EndsWith("ssg_SearchRequestSubmittoQueueActions", StringComparison.InvariantCultureIgnoreCase))
+                    request.Content = new StringContent(string.Empty,
+                                    Encoding.UTF8,
+                                    "application/json");//CONTENT-TYPE header
+
                 request.RequestUri = path;
             }
 

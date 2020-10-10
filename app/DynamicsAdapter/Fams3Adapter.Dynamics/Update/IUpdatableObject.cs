8866740557a0c1abab90a9fd9f4df7582a7c8b38
PeggyZhangNtt <190728@nttdata.com>
2020-10-09 17:39:43 -0700

    update unit tests

--- app/DynamicsAdapter/Fams3Adapter.Dynamics/Update/IUpdatableObject.cs
+++ app/DynamicsAdapter/Fams3Adapter.Dynamics/Update/IUpdatableObject.cs
@@ -13,63 +13,7 @@ namespace Fams3Adapter.Dynamics.Update
 
     public static class IUpdatableObjectExtensions
     {
-        /// <summary>
-        /// this method is not used in this project.
-        /// </summary>
-        /// <typeparam name="IUpdatableObject"></typeparam>
-        /// <param name="originObj"></param>
-        /// <param name="newObj"></param>
-        /// <returns></returns>
-        public static IUpdatableObject MergeUpdates<IUpdatableObject>(this IUpdatableObject originObj, object newObj)
-        {
-            if (newObj == null || originObj == null) return originObj;
 
-            Type newType = newObj.GetType();
-            bool updated = false;
-            IList<PropertyInfo> props = new List<PropertyInfo>(newType.GetProperties());
-            foreach (PropertyInfo propertyInfo in props)
-            {
-                if (Attribute.IsDefined(propertyInfo, typeof(UpdateIgnoreAttribute))) continue;
-
-                var newValue = propertyInfo.GetValue(newObj, null);
-                var oldValue = propertyInfo.GetValue(originObj, null);
-
-                if (newValue != null)
-                {
-                    bool isDifferent = !string.Equals(newValue.ToString(), oldValue == null ? "null" : oldValue.ToString(), StringComparison.InvariantCultureIgnoreCase);
-                    if (isDifferent)
-                    {
-                        if (propertyInfo.PropertyType.Name == "Boolean")
-                        {
-                            propertyInfo.SetValue(originObj, newValue);
-                            updated = true;
-                        }
-                        else if (propertyInfo.PropertyType.Name == "String")
-                        {
-                            if (!string.IsNullOrEmpty((string)newValue))//new value is null, no matter old value has value or not, we do not change the old value
-                            {
-                                propertyInfo.SetValue(originObj, newValue);
-                                updated = true;
-                            }
-                        }
-                        else
-                        {
-                            propertyInfo.SetValue(originObj, newValue);
-                            updated = true;
-                        }
-                    }
-                    Type[] t = propertyInfo.PropertyType.GetInterfaces();
-                    if (propertyInfo.PropertyType.GetInterfaces().SingleOrDefault(m => m.Name == "IUpdatableObject") != null)
-                    {
-                        propertyInfo.SetValue(originObj, MergeUpdates(oldValue, newValue));
-                    }
-                }
-            }
-            PropertyInfo pi = originObj.GetType().GetProperties().SingleOrDefault(m => m.Name == "Updated");
-            if (pi == null) return originObj;
-            else pi.SetValue(originObj, updated);
-            return originObj;
-        }
 
         public static IDictionary<string, object> GetUpdateEntries<IUpdatableObject>(this IUpdatableObject originObj, object newObj)
         {

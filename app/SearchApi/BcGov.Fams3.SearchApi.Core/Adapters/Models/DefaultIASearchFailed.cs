110b7754cb8edc72852654d8c73d926023ca4517
Ranti Familusi <ranti.familusi@nttdata.com>
2020-12-09 22:12:30 -0700

    fix

--- app/SearchApi/BcGov.Fams3.SearchApi.Core/Adapters/Models/DefaultIASearchFailed.cs
+++ app/SearchApi/BcGov.Fams3.SearchApi.Core/Adapters/Models/DefaultIASearchFailed.cs
@@ -20,13 +20,15 @@ namespace BcGov.Fams3.SearchApi.Core.Adapters.Models
         public string SearchRequestKey { get; }
         public DateTime TimeStamp { get; }
 
-        public Person Person { get;  }
+        public Person Person { get; }
 
-        public string BatchNo { get;}
+        public string BatchNo { get; }
 
         public bool Retry { get; }
 
         public string Cause { get; }
+
+        public string RequestorTimeStamp { get; }
+
     }
-    
 }

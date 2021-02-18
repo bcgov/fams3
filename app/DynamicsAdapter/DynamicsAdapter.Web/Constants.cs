75def58814f15ce67f1035129d8df0b44882a198
PeggyZhangNtt <190728@nttdata.com>
2021-02-18 13:51:02 -0800

    fix cnl issue
    fix realestate could not locate mapping
    
    
    respond to comments
    
    
    new line
    
    
    new line
    
    
    new line
    
    
    new line
    
    
    new line

---
+++ app/DynamicsAdapter/DynamicsAdapter.Web/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DynamicsAdapter.Web
+{
+    public static class Constants
+    {
+        public const string CouldNotLocate = "Could Not Locate";
+    }
+}
+

e30ed26c4c33f4f2e69f9f32f64b899f86fc4795
PeggyZhangNtt <190728@nttdata.com>
2021-02-26 16:09:52 -0800

    update contract

---
+++ app/SearchApi/BcGov.Fams3.SearchApi.Contracts/SearchRequest/NotificationAcknowledged.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BcGov.Fams3.SearchApi.Contracts.SearchRequest
+{
+    public interface NotificationAcknowledged
+    {
+        string RequestorReference { get; set; }
+        NotificationType NotificationType { get; set; }
+        string FileId { get; set; }
+        NotificationStatusEnum Status { get; set; }
+        string AgencyCode { get; set; }
+        DateTime TimeStamp { get; }
+    }
+
+    public enum NotificationStatusEnum
+    {
+        SUCCESS,
+        FAIL
+    }
+}

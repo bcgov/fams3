40ef75f6af9707371ed8c24ececbb89f9e72901b
PeggyZhangNtt <190728@nttdata.com>
2021-06-08 12:09:49 -0700

    add serviceName to Dynadaptor logging

--- app/DynamicsAdapter/DynamicsAdapter.Web/Program.cs
+++ app/DynamicsAdapter/DynamicsAdapter.Web/Program.cs
@@ -41,12 +41,14 @@ namespace DynamicsAdapter.Web
             Host.CreateDefaultBuilder(args)
                 .UseSerilog((hostingContext, loggerConfiguration) =>
                 {
+                    string serviceName = hostingContext.Configuration["JAEGER_SERVICE_NAME"];
                     loggerConfiguration
                         .ReadFrom.Configuration(hostingContext.Configuration)
                         .Enrich.WithPropertySearchRequestKey("SearchRequestKey")
                         .Enrich.WithPropertyDataPartner("DataPartner")
                         .Enrich.WithPropertyDataPartner("AgencyCode")
                         .Enrich.WithPropertyRequestRef("RequestRef")
+                        .Enrich.WithProperty("ServiceName", serviceName)
                         .Enrich.FromLogContext();
 
                     string splunkCollectorUrl = hostingContext.Configuration["SPLUNK_COLLECTOR_URL"];

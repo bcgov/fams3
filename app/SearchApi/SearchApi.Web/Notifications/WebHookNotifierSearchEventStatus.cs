d2df24ffca5d9097e602b3e45ee4f354599f37b9
PeggyZhangNtt <190728@nttdata.com>
2021-06-09 17:48:38 -0700

    autosearch , webhook error, put into error queue.

--- app/SearchApi/SearchApi.Web/Notifications/WebHookNotifierSearchEventStatus.cs
+++ app/SearchApi/SearchApi.Web/Notifications/WebHookNotifierSearchEventStatus.cs
@@ -106,6 +106,7 @@ namespace SearchApi.Web.Notifications
             }
             catch (Exception)
             {
+                _logger.LogError("The webhook {eventName} is failed and is put into error queue.", eventName);
                 throw;
             }
             finally
